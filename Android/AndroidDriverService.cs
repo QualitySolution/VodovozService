7ec4bb5d861338b852c026cd7adba04503dc15cd
Andrei <anri.vin@gmail.com>
2016-06-07 16:16:35 +0300

    Added stub-methods for auth, auth check and etc.

--- Android/AndroidDriverService.cs
+++ Android/AndroidDriverService.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Android
 {
@@ -8,13 +13,53 @@ namespace Android
 
 		public string Auth (string login, string password)
 		{
-			Console.WriteLine ("login:{0} password:{1}", login, password);
-			if (login == "vad" && password == "123")
-				return "SuC3ss";
+			//TODO: Replace with real logic
+			Console.WriteLine("Auth called with args:\nlogin: {0}\npassword: {1}", login, password);
+			var hash = (new SHA1Managed()).ComputeHash(Encoding.UTF8.GetBytes("123"));
+			string str = string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+			if (login == "vad" && password == str)
+				return "74d5607f28d9051a5dc43h2jfe7dc8eb";
 			else
-				return "Fa1lure";
+				return null;
 		}
 
+		public bool CheckAuth (string authKey)
+		{
+			//TODO: Replace with real logic
+			Console.WriteLine("CheckAuth called with args:\nauthKey: {0}", authKey);
+			if (authKey == "74d5607f28d9051a5dc43h2jfe7dc8eb")
+				return true;
+			return false;
+		}
+
+		public string GetRouteLists (string authKey)
+		{
+			//TODO: Replace with real logic
+			Console.WriteLine("GetRouteLists called with args:\nauthKey: {0}", authKey);
+			if (!CheckAuth (authKey))
+				return null;
+			List<string> routeLists = new List<string> ();
+			routeLists.Add ("Маршрутный лист 1");
+			routeLists.Add ("Маршрутный лист 2");
+			return JsonConvert.SerializeObject (routeLists);		
+		}
+
+		public string GetRouteListOrders (string authKey, int routeListId)
+		{
+			//TODO: Replace with real logic
+			Console.WriteLine("GetRouteListOrders called with args:\nauthKey: {0}\nrouteListId: {1}", authKey, routeListId);
+			if (!CheckAuth (authKey))
+				return null;
+			List<string> orders = new List<string> ();
+			if (routeListId == 1) {
+				orders.Add ("Заказ 1");
+				orders.Add ("Заказ 2");
+			} else {
+				orders.Add ("Заказ 3");
+				orders.Add ("Заказ 4");
+			}
+			return JsonConvert.SerializeObject (orders);
+		}
 		#endregion
 	}
 }
--- Android/IAndroidDriverService.cs
+++ Android/IAndroidDriverService.cs
@@ -10,6 +10,18 @@ namespace Android
 		[OperationContract]
 		[WebInvoke (UriTemplate = "/Auth", BodyStyle = WebMessageBodyStyle.WrappedRequest)] 
 		string Auth (string login, string password);
+
+		[OperationContract]
+		[WebInvoke (UriTemplate = "/CheckAuth", BodyStyle = WebMessageBodyStyle.WrappedRequest)] 
+		bool CheckAuth (string authKey);
+
+		[OperationContract]
+		[WebInvoke (UriTemplate = "/GetRouteLists", BodyStyle = WebMessageBodyStyle.WrappedRequest)] 
+		string GetRouteLists (string authKey);
+
+		[OperationContract]
+		[WebInvoke (UriTemplate = "/GetRouteListOrders", BodyStyle = WebMessageBodyStyle.WrappedRequest)] 
+		string GetRouteListOrders (string authKey, int routeListId);
 	}
 }
 
