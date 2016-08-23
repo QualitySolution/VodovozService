ec2658e81b9f54f45de7fb79b87f8beac2459891
Andrei <anri.vin@gmail.com>
2016-08-23 10:22:40 +0300

    Updated android version.

--- Android/AndroidDriverService.cs
+++ Android/AndroidDriverService.cs
@@ -20,7 +20,7 @@ namespace Android
 		/// Const value, equals to android code version on AndroidManifest.xml
 		/// Needed for version checking. Increment this value on each API change.
 		/// </summary>
-		private const int VERSION_CODE = 7;
+		private const int VERSION_CODE = 8;
 
 		#region IAndroidDriverService implementation
 
