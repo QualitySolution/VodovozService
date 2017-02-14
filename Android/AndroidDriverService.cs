684dd7bfee3b3c09ed89d320ee211d94d59ee055
Andrey Gankov <gav@qsolution.ru>
2017-02-14 18:25:38 +0300

    Версия андройд 10.

--- Android/AndroidDriverService.cs
+++ Android/AndroidDriverService.cs
@@ -23,7 +23,7 @@ namespace Android
 		/// Const value, equals to android code version on AndroidManifest.xml
 		/// Needed for version checking. Increment this value on each API change.
 		/// </summary>
-		private const int VERSION_CODE = 9;
+		private const int VERSION_CODE = 10;
 
 		#region IAndroidDriverService implementation
 
