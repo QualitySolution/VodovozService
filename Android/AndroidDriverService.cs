ab3bb38984029a90487c1ce3bb9e48c79cfd3da8
Andrey Gankov <gav@qsolution.ru>
2017-03-15 20:21:48 +0300

    Переписал получение координат на отдельные обработчики, с кеширование треков в сесси хебернейта, чтобы не дергать базу каждые несколько секунд.

--- Android/AndroidDriverService.cs
+++ Android/AndroidDriverService.cs
@@ -213,77 +213,10 @@ namespace Android
 			if (prop != null)
 				logger.Info("Получены координаты по треку {0} c ip{1}:{2}", trackId, prop.Address, prop.Port);
 
-			DateTime startOp = DateTime.Now;
-
-			var DecimalSeparatorFormat = new NumberFormatInfo { NumberDecimalSeparator = ".", NumberGroupSeparator = "," };
-			var CommaSeparatorFormat = new NumberFormatInfo { NumberDecimalSeparator = ",", NumberGroupSeparator = "." };
-
 			if (!CheckAuth (authKey))
 				return false;
 
-			var trackUoW = UnitOfWorkFactory.CreateForRoot<Track>(trackId);
-			if (trackUoW == null || trackUoW.Root == null)
-				return false;
-
-			try
-			{
-				Vodovoz.Domain.Logistic.TrackPoint lastTp = null;
-				foreach (TrackPoint tp in TrackPointList) {
-					var trackPoint = new Vodovoz.Domain.Logistic.TrackPoint();
-					Double Latitude, Longitude;
-					if (!Double.TryParse(tp.Latitude, NumberStyles.Float, DecimalSeparatorFormat, out Latitude) 
-						&& !Double.TryParse(tp.Latitude, NumberStyles.Float, CommaSeparatorFormat, out Latitude))
-					{
-						logger.Warn("Не получилось разобрать координату широты: {0}", tp.Latitude);
-						return false;
-					}
-					if (!Double.TryParse(tp.Longitude, NumberStyles.Float, DecimalSeparatorFormat, out Longitude) 
-						&& !Double.TryParse(tp.Longitude, NumberStyles.Float, CommaSeparatorFormat, out Longitude))
-					{
-						logger.Warn("Не получилось разобрать координату долготы: {0}", tp.Longitude);
-						return false;
-					}
-					trackPoint.Latitude = Latitude;
-					trackPoint.Longitude = Longitude;
-					trackPoint.TimeStamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(long.Parse(tp.TimeStamp)).ToLocalTime();
-					//Округляем время до секунд.
-					trackPoint.TimeStamp = new DateTime(trackPoint.TimeStamp.Year, trackPoint.TimeStamp.Month, trackPoint.TimeStamp.Day, trackPoint.TimeStamp.Hour, trackPoint.TimeStamp.Minute, trackPoint.TimeStamp.Second);
-					if(lastTp != null && lastTp.TimeStamp == trackPoint.TimeStamp)
-					{
-						logger.Warn("Для секунды {0} трека {1}, присутствует вторая пара координат, пропускаем что бы округлить трек с точностью не чаще раза в секунду.", trackPoint.TimeStamp, trackId);
-						continue;
-					}
-					lastTp = trackPoint;
-					var existPoint = trackUoW.Root.TrackPoints.FirstOrDefault(x => x.TimeStamp == trackPoint.TimeStamp);
-					if(existPoint != null)
-					{
-						if(Math.Abs(existPoint.Latitude - trackPoint.Latitude) < 0.00000001 && Math.Abs(existPoint.Longitude - trackPoint.Longitude) < 0.00000001)
-						{
-							logger.Warn("Координаты на время {0} для трека {1}, были получены повторно поэтому пропущены.", trackPoint.TimeStamp, existPoint.Track.Id);
-						}
-						else
-						{
-							logger.Warn($"Координаты на время {trackPoint.TimeStamp} для трека {existPoint.Track.Id}, были получены повторно и изменены " +
-								$"lat: {existPoint.Latitude} -> {trackPoint.Latitude} log: {existPoint.Longitude} -> {trackPoint.Longitude}");
-							existPoint.Latitude = trackPoint.Latitude ;
-							existPoint.Longitude = trackPoint.Longitude ;
-						}
-					}
-					else
-					{
-						trackPoint.Track = trackUoW.Root;
-						trackUoW.Root.TrackPoints.Add(trackPoint);
-					}
-				}
-				trackUoW.Save();
-				logger.Info("Закончена обработка координат для трека {0} за {1} сек.", trackId, (DateTime.Now - startOp).TotalSeconds);
-				return true;
-			}
-			catch (Exception e)
-			{
-				logger.Error(e, "На обоработке трека {0}", trackId);
-			}
-			return false;
+			return TracksService.ReceivedCoordinates(trackId, TrackPointList);
 		}
 
 		public bool ChangeOrderStatus (string authKey, int orderId, string status, int? bottlesReturned) {
---
+++ Android/TrackMaintainer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using QSOrmProject;
+using Vodovoz.Domain.Logistic;
+using System.Globalization;
+using System.Linq;
+
+namespace Android
+{
+	public  class TrackMaintainer
+	{
+		static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
+
+		IUnitOfWorkGeneric<Track> uow;
+
+		public bool IsBusy { get; private set;}
+
+		public Track Track {get{ return uow.Root;}}
+
+		public TrackMaintainer(int trackId)
+		{
+			uow = UnitOfWorkFactory.CreateForRoot<Track>(trackId);
+		}
+
+		public bool SaveNewCoordinates(TrackPointList trackPointList)
+		{
+			IsBusy = true;
+			DateTime startOp = DateTime.Now;
+			var DecimalSeparatorFormat = new NumberFormatInfo { NumberDecimalSeparator = ".", NumberGroupSeparator = "," };
+			var CommaSeparatorFormat = new NumberFormatInfo { NumberDecimalSeparator = ",", NumberGroupSeparator = "." };
+
+			Vodovoz.Domain.Logistic.TrackPoint lastTp = null;
+			foreach (TrackPoint tp in trackPointList) {
+				var trackPoint = new Vodovoz.Domain.Logistic.TrackPoint();
+				Double Latitude, Longitude;
+				if (!Double.TryParse(tp.Latitude, NumberStyles.Float, DecimalSeparatorFormat, out Latitude) 
+					&& !Double.TryParse(tp.Latitude, NumberStyles.Float, CommaSeparatorFormat, out Latitude))
+				{
+					logger.Warn("Не получилось разобрать координату широты: {0}", tp.Latitude);
+					IsBusy = false;
+					return false;
+				}
+				if (!Double.TryParse(tp.Longitude, NumberStyles.Float, DecimalSeparatorFormat, out Longitude) 
+					&& !Double.TryParse(tp.Longitude, NumberStyles.Float, CommaSeparatorFormat, out Longitude))
+				{
+					logger.Warn("Не получилось разобрать координату долготы: {0}", tp.Longitude);
+					IsBusy = false;
+					return false;
+				}
+				trackPoint.Latitude = Latitude;
+				trackPoint.Longitude = Longitude;
+				trackPoint.TimeStamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(long.Parse(tp.TimeStamp)).ToLocalTime();
+				//Округляем время до секунд.
+				trackPoint.TimeStamp = new DateTime(trackPoint.TimeStamp.Year, trackPoint.TimeStamp.Month, trackPoint.TimeStamp.Day, trackPoint.TimeStamp.Hour, trackPoint.TimeStamp.Minute, trackPoint.TimeStamp.Second);
+				if(lastTp != null && lastTp.TimeStamp == trackPoint.TimeStamp)
+				{
+					logger.Warn("Для секунды {0} трека {1}, присутствует вторая пара координат, пропускаем что бы округлить трек с точностью не чаще раза в секунду.", trackPoint.TimeStamp, Track.Id);
+					continue;
+				}
+				lastTp = trackPoint;
+				var existPoint = Track.TrackPoints.FirstOrDefault(x => x.TimeStamp == trackPoint.TimeStamp);
+				if(existPoint != null)
+				{
+					if(Math.Abs(existPoint.Latitude - trackPoint.Latitude) < 0.00000001 && Math.Abs(existPoint.Longitude - trackPoint.Longitude) < 0.00000001)
+					{
+						logger.Warn("Координаты на время {0} для трека {1}, были получены повторно поэтому пропущены.", trackPoint.TimeStamp, existPoint.Track.Id);
+					}
+					else
+					{
+						logger.Warn($"Координаты на время {trackPoint.TimeStamp} для трека {existPoint.Track.Id}, были получены повторно и изменены " +
+							$"lat: {existPoint.Latitude} -> {trackPoint.Latitude} log: {existPoint.Longitude} -> {trackPoint.Longitude}");
+						existPoint.Latitude = trackPoint.Latitude ;
+						existPoint.Longitude = trackPoint.Longitude ;
+					}
+				}
+				else
+				{
+					trackPoint.Track = Track;
+					Track.TrackPoints.Add(trackPoint);
+				}
+			}
+			uow.Save();
+			logger.Info("Обработаны координаты для трека {0} за {1} сек.", Track.Id, (DateTime.Now - startOp).TotalSeconds);
+			IsBusy = false;
+			return true;
+		}
+	}
+}
+
