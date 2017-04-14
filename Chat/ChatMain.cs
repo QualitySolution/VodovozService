5f60f2fa5eeafda42c079cb9f69724b2d6257e8b
Andrey Gankov <gav@qsolution.ru>
2017-04-14 17:02:21 +0300

    Добавлена возможность хранить общие настройки.

---
+++ Chat/ChatMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Chat
+{
+	public static class ChatMain
+	{
+		public static string ChatServer { get; set; }
+		
+		public static string ChatServiceUrl{
+			get{
+				return $"http://{ChatServer}/ChatService";
+			}
+		}
+	}
+}
