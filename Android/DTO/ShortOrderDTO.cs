d2d8c0ee1b92d6169d88b470a7c26f9ebfbb1471
Andrey Gankov <gav@qsolution.ru>
2017-04-17 10:23:21 +0300

    Возвращаем не null, а пустую строку, чтобы не падал андройд.

--- Android/DTO/ShortOrderDTO.cs
+++ Android/DTO/ShortOrderDTO.cs
@@ -30,9 +30,9 @@ namespace Android
 		public ShortOrderDTO (RouteListItem item)
 		{
 			Id = item.Order.Id;
-			DeliverySchedule = item.Order?.DeliverySchedule?.DeliveryTime;
+			DeliverySchedule = item.Order?.DeliverySchedule?.DeliveryTime ?? String.Empty;
 			OrderStatus = item.Status.GetEnumTitle();
-			Counterparty = item.Order?.Client?.FullName;
+			Counterparty = item.Order?.Client?.FullName ?? String.Empty;
 			Address = item.Order?.DeliveryPoint?.ShortAddress ?? String.Empty;
 		}
 	}
