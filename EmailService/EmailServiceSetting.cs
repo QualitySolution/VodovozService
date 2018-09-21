5a9d47a9f943ea9b6e81e10ecce55b6b0da54374
Vadim Gordeev <enzogord@gmail.com>
2018-09-21 19:46:32 +0300

    Проверка возможности отправки почты если не настроен сервис

--- EmailService/EmailServiceSetting.cs
+++ EmailService/EmailServiceSetting.cs
@@ -4,10 +4,15 @@ namespace EmailService
 {
 	public static class EmailServiceSetting
 	{
+		public static bool CanSendEmail => !string.IsNullOrWhiteSpace(EmailServiceURL);
+
 		public static string EmailServiceURL { get; set; }
 
 		public static IEmailService GetEmailService()
 		{
+			if(!CanSendEmail) {
+				return null;
+			}
 			return new ChannelFactory<IEmailService>(new BasicHttpBinding(), string.Format("http://{0}/EmailService", EmailServiceURL))
 				.CreateChannel();
 		}
