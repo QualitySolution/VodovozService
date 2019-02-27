aed112281f86453fc9ea7e8d2e10b0d5f58a9f2e
Vadim Gordeev <enzogord@gmail.com>
2019-02-27 16:27:16 +0300

    Изменены настройки подключения к серверу отправки почты, для большего контроля

--- EmailService/EmailServiceSetting.cs
+++ EmailService/EmailServiceSetting.cs
@@ -2,19 +2,34 @@
 
 namespace EmailService
 {
-	public static class EmailServiceSetting
+	public class EmailServiceSetting
 	{
-		public static bool CanSendEmail => !string.IsNullOrWhiteSpace(EmailServiceURL);
+		private static EmailServiceSetting settingInstance;
 
-		public static string EmailServiceURL { get; set; }
+		public static bool SendingAllowed => settingInstance != null;
 
 		public static IEmailService GetEmailService()
 		{
-			if(!CanSendEmail) {
+			if(!SendingAllowed) {
 				return null;
 			}
-			return new ChannelFactory<IEmailService>(new BasicHttpBinding(), string.Format("http://{0}/EmailService", EmailServiceURL))
+			return new ChannelFactory<IEmailService>(new BasicHttpBinding(), string.Format("http://{0}/EmailService", settingInstance.serviceUrl))
 				.CreateChannel();
+		}
+
+		public static void Init(string serviceUrl)
+		{
+			if(string.IsNullOrWhiteSpace(serviceUrl)) {
+				return;
+			}
+			settingInstance = new EmailServiceSetting(serviceUrl);
+		}
+
+		private string serviceUrl;
+
+		private EmailServiceSetting(string serviceUrl)
+		{
+			this.serviceUrl = serviceUrl;
 		}
 	}
 }
