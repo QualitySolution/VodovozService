8b9908ac4341977d3a9d4bb677102847c110a3db
Vadim Gordeev <enzogord@gmail.com>
2019-05-15 11:41:34 +0300

    В скрипт обновления добавлен новый порт для подключения по ssh

--- VodovozService/UploadToServer.sh
+++ VodovozService/UploadToServer.sh
@@ -4,18 +4,18 @@ echo "1) Release"
 echo "2) Debug"
 read case;
 
-ssh root@vod-srv.qsolution.ru "systemctl stop vodovozservice"
+ssh root@vod-srv.qsolution.ru -p2201 "systemctl stop vodovozservice"
 
 #Удаляем старый лог.
 #ssh root@vod-srv.qsolution.ru "sudo rm /var/log/VodovozService/server.log"
 
 case $case in
     1)
-rsync -vizaP --delete ./bin/Release/ root@vod-srv.qsolution.ru:/opt/VodovozService
+rsync -vizaP --delete -e "ssh -p 2201" ./bin/Release/ root@vod-srv.qsolution.ru:/opt/VodovozService
 ;;
     2)
-rsync -vizaP --delete ./bin/Debug/ root@vod-srv.qsolution.ru:/opt/VodovozService
+rsync -vizaP --delete -e "ssh -p 2201" ./bin/Debug/ root@vod-srv.qsolution.ru:/opt/VodovozService
 ;;
 esac
 
-ssh root@vod-srv.qsolution.ru "sudo systemctl start vodovozservice"
+ssh root@vod-srv.qsolution.ru -p2201 "sudo systemctl start vodovozservice"
