80fa32aa983cb99f0a3736e5359725e93acc30e1
Vadim Gordeev <enzogord@gmail.com>
2019-08-16 17:58:20 +0300

    Скрипт обновления служб на сервера

---
+++ UploadToServer.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+echo "Какие службы необходимо обновить?"
+echo "1) Driver"
+echo "2) Email"
+echo "3) Mobile"
+echo "4) OSM"
+echo "5) Old server DriverMobileGroup"
+echo "Можно вызывать вместе, например Driver+Email=12"
+read service;
+
+echo "Какую сборку использовать?"
+echo "1) Release"
+echo "2) Debug"
+read build;
+
+driverServiceFolder="VodovozAndroidDriverService"
+driverServiceName="vodovoz-driver.service"
+
+emailServiceFolder="VodovozEmailService"
+emailServiceName="vodovoz-email.service"
+
+mobileServiceFolder="VodovozMobileService"
+mobileServiceName="vodovoz-mobile.service"
+
+osmServiceFolder="VodovozOSMService"
+osmServiceName="vodovoz-osm.service"
+
+serverAddress="admin@192.168.0.16"
+serverPort="22"
+
+
+# ------ для объединенной службы водителей и мобильного приложения для старого сервера
+
+oldServerAddress="root@vod.qsolution.ru"
+oldServerPort="2201"
+
+driverMobileGroupServiceFolder="VodovozDriverAndMobileServiceGroup"
+driverMobileGroupServiceName="vodovoz-driver-mobile-group.service"
+
+# ------------------------------
+
+buildFolderName=""
+case $build in
+	1)
+		buildFolderName="Release"
+	;;
+	2)
+		buildFolderName="Debug"
+	;;
+esac
+
+function DeleteHttpDll {
+	deletedFilePath="./$1/bin/$buildFolderName/System.Net.Http.dll"
+
+	echo "-- Delete incorrect generated files: $deletedFilePath"
+
+	if [ -f $deletedFilePath ]
+		then rm $deletedFilePath
+	fi
+}
+
+function CopyFiles {
+	rsync -vizaP --delete -e "ssh -p $serverPort" ./$1/bin/$buildFolderName/ $serverAddress:/opt/$1
+}
+
+function UpdateDriverService {
+	printf "\nОбновление службы водителей\n"
+
+	echo "-- Stoping $driverServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl stop $driverServiceName
+
+	echo "-- Copying $driverServiceName files"
+	DeleteHttpDll $driverServiceFolder
+	CopyFiles $driverServiceFolder
+
+	echo "-- Starting $driverServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl start $driverServiceName
+}
+
+function UpdateEmailService {
+	printf "\nОбновление службы отправки электронной почты\n"
+
+	echo "-- Stoping $emailServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl stop $emailServiceName	
+
+	echo "-- Copying $emailServiceName files"
+	DeleteHttpDll $emailServiceFolder
+	CopyFiles $emailServiceFolder
+
+	echo "-- Starting $emailServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl start $emailServiceName
+}
+
+function UpdateMobileService {
+	printf "\nОбновление службы мобильного приложения\n"
+
+	echo "-- Stoping $mobileServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl stop $mobileServiceName	
+
+	echo "-- Copying $mobileServiceName files"
+	DeleteHttpDll $mobileServiceFolder
+	CopyFiles $mobileServiceFolder
+
+	echo "-- Starting $mobileServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl start $mobileServiceName
+}
+
+function UpdateOSMService {
+	printf "\nОбновление службы OSM\n"
+
+	echo "-- Stoping $osmServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl stop $osmServiceName
+
+	echo "-- Copying $osmServiceName files"
+	DeleteHttpDll $osmServiceFolder
+	CopyFiles $osmServiceFolder
+
+	echo "-- Starting $osmServiceName"
+	ssh $serverAddress -p$serverPort sudo systemctl start $osmServiceName
+}
+
+function UpdateDriverMobileGroupService {
+	printf "\nОбновление службы для водителей и мобильного приложения на старом сервере\n"
+
+	echo "-- Stoping $driverMobileGroupServiceName"
+	ssh $oldServerAddress -p$oldServerPort sudo systemctl stop $driverMobileGroupServiceName
+
+	echo "-- Copying $driverMobileGroupServiceName files"
+	DeleteHttpDll $driverMobileGroupServiceFolder
+	CopyFiles $driverMobileGroupServiceFolder
+
+	echo "-- Starting $driverMobileGroupServiceName"
+	ssh $oldServerAddress -p$oldServerPort sudo systemctl start $driverMobileGroupServiceName
+}
+
+case $service in
+	*1*)
+		UpdateDriverService
+	;;&
+	*2*)
+		UpdateEmailService
+	;;&
+	*3*)
+		UpdateMobileService
+	;;&
+	*4*)
+		UpdateOSMService
+	;;&
+	*5*)
+		UpdateDriverMobileGroupService
+	;;
+esac
+
+read -p "Press enter to exit"
