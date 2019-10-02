318b9eb851473477b80696f7d6ee798f259c8962
Vadim Gordeev <enzogord@gmail.com>
2019-10-02 17:23:41 +0300

    Добавлен интерфейс для отправки смс сообщений

---
+++ VodovozSmsInformerService/SmsSendInterface/BalanceType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SmsSendInterface
+{
+	/// <summary>
+	/// Тип баланса
+	/// </summary>
+	public enum BalanceType
+	{
+		/// <summary>
+		/// Количество смс сообщений
+		/// </summary>
+		SmsCounts,
+
+		/// <summary>
+		/// Баланс в валюте
+		/// </summary>
+		CurrencyBalance
+	}
+}
---
+++ VodovozSmsInformerService/SmsSendInterface/SmsBalanceEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SmsSendInterface
+{
+	public class SmsBalanceEventArgs : EventArgs
+	{
+		BalanceType BalanceType { get; }
+		decimal Balance { get; }
+
+		public SmsBalanceEventArgs(BalanceType balanceType, decimal balance)
+		{
+			BalanceType = balanceType;
+			Balance = balance;
+		}
+	}
+}
